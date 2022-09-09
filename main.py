3e0e33b3c2efda9fe933836c7011aff3554b22de
Omar Palmer-Lopez <opalmerlopez@gmail.com>
2022-09-09 06:32:07 +0100

    Updated main.py and app.yaml

--- main.py
+++ main.py
@@ -10,5 +10,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
